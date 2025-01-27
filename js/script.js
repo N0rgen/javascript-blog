932f7c8ad3e786731a1f01b29a1eebbf0975f603
Kamil <norgen18@wp.pl>
2025-01-27 17:11:28 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
   }); */
   
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
   
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -16,6 +16,8 @@
     }
   
     /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
   
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
