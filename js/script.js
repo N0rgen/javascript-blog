9f6807d9f820cd857c1670fa98f94bfb8f70ae34
Kamil <norgen18@wp.pl>
2025-01-28 15:55:43 +0100

    final left menu js version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-'use strict';
+{
+  'use strict';
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -29,18 +30,57 @@
   
     /* get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href')
-    console.log(articleSelector);
+    
   
     /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector)
-    console.log(targetArticle);
+    
   
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
   
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+    for (article of articles) {
+
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+
+      /* find the title element */
+      /* get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      
+
+      /* insert link into titleList */
+      html = html + linkHTML;
+
+    }
+    titleList.innerHTML = html;
+    
+  }
+
+  generateTitleLinks();
   const links = document.querySelectorAll('.titles a');
+  console.log(links)
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+}
